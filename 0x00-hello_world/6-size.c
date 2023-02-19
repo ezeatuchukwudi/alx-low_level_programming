a2f5e5f3050ef0086b8cd07a539173e1d30f9103
ezeatuchukwudi <scodemy@gmail.com>
2023-02-18 23:15:47 -0800

    Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+
+int main() {
+  printf("Size of char: %ld byte(s)\n", sizeof(char));
+  printf("Size of short: %ld byte(s)\n", sizeof(short));
+  printf("Size of int: %ld byte(s)\n", sizeof(int));
+  printf("Size of long: %ld byte(s)\n", sizeof(long));
+  printf("Size of long long: %ld byte(s)\n", sizeof(long long));
+  printf("Size of float: %ld byte(s)\n", sizeof(float));
+  printf("Size of double: %ld byte(s)\n", sizeof(double));
+  printf("Size of long double: %ld byte(s)\n", sizeof(long double));
+  printf("Size of pointer: %ld byte(s)\n", sizeof(void *));
+  return 0;
 }
+
