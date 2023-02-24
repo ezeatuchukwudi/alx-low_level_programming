8a46186485912bc377a0dd323c018ed67582c1dd
ezeatuchukwudi <scodemy@gmail.com>
2023-02-24 05:42:07 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Entry point
+ *
+ * Description: 'writes the character c to stdout'
+ * @c: the character to print
+ *
+ * Return : On success 1.
+ * On error, -1 is returned, and error is set approperately.
+ */
+
+int _putchar(char c)
+{
+
+return (write(1, &c, 1));
+}
